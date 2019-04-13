96d6f495fb914c1676b9689e9c6ef48401655502
肖鹏 <731574722@qq.com>
2019-04-13 18:11:33 +0800

    update

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 
-vector<int> vec;
 using namespace std;
 
+vector<int> vec;
 int main(int argc, char const *argv[])
 {
     int a, b;
     cin >> a >> b;
+    vec.push_back(a);    
     cout << a + b << endl;
     
     return 0;
